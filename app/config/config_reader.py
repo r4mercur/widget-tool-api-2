b309b5c7aa01114874b1307029328db73e439a61
r4mercur <bjarnemb9@gmail.com>
2024-05-21 12:53:10 +0000

    update config reader

--- app/config/config_reader.py
+++ app/config/config_reader.py
@@ -3,17 +3,18 @@ import yaml
 
 from dotenv import load_dotenv
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+load_dotenv(dotenv_path=dotenv_path)
 
-# Load the config file
+# Load the config file and store it in a global variable
 with open('config/config.yaml', 'r') as f:
     global config
     config = yaml.safe_load(f)
 
 # Replace the environment variables in the config file
 config['database_uri'] = os.environ['DATABASE_URI']
-config['http_user'] = os.environ['HTTP_USER']
-config['http_password'] = os.environ['HTTP_PASSWORD']
+config['general_sharing_settings']['http_user'] = os.environ['HTTP_USER']
+config['general_sharing_settings']['http_password'] = os.environ['HTTP_PASSWORD']
 
 # Save the updated config file
 with open("config/config.yaml", "w") as f:
